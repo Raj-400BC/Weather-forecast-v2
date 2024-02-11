0d2d84d2848764916034b84e4b715d62af2d4e2e
Mayur Yaduvanshi <singhmayurraj97@gmail.com>
2024-02-11 18:16:14 +0530

    Rendered Leaflet map through code to ensure visibility and functionality.

--- public/sketch.js
+++ public/sketch.js
@@ -1,3 +1,6 @@
+// const map = L.map("map").setView([0, 0],2);   // using this to load map even if the has not clicked the FindMe button
+
+
 document.addEventListener('DOMContentLoaded', function setup() {
     const button = document.getElementById('submit');
     button.addEventListener("click", async (event) => {
@@ -6,9 +9,34 @@ document.addEventListener('DOMContentLoaded', function setup() {
         const longitude = position.coords.longitude;
         document.getElementById("latitude").textContent = latitude;
         document.getElementById("longitude").textContent = longitude;
-        // const Api = process.env.API_KEY;
-        // const response = await fetch(Api);
-        // const json = await response.json();
+        
+///////     Leaflet.js start
+        const map = L.map("map").setView([0, 0],2);
+
+        const attribution =
+          '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>';
+        const tileURL = "https://tile.openstreetmap.org/{z}/{x}/{y}.png";
+        const tiles = L.tileLayer(tileURL, { attribution });
+        tiles.addTo(map);
+        
+        // Making marker and custom icon
+        var myIcon = L.icon({
+          iconUrl: "/assets/images/marker.png",
+          iconSize: [50, 32],
+          iconAnchor: [25, 16],
+        });
+        const marker = L.marker([0, 0], { icon: myIcon }).addTo(map);
+        
+        let firstTime = true; // to stop the zooming on each lat and lon update
+        marker.setLatLng([latitude, longitude]);
+
+        if (firstTime) {
+          map.setView([latitude, longitude], 3);
+          firstTime = false;
+        }
+    
+///////     Leaflet.js end
+
         const data = { latitude, longitude };
       const options = {
         method: "POST",
@@ -22,6 +50,7 @@ document.addEventListener('DOMContentLoaded', function setup() {
       console.log(json);
         // document.getElementById("lat").textContent = json.coord.lat.toFixed(2);
         // document.getElementById("lon").textContent = json.coord.lon.toFixed(2);
+        document.getElementById("currentLocation").textContent = json.name;
         document.getElementById("temp").textContent = json.main.temp;
         document.getElementById("speed").textContent = json.wind.speed;
         document.getElementById("weather").textContent = json.weather[0].main;
